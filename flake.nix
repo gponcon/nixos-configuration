4c6d40c7621efacc0f490fd04dbd308bce69a1f3
Guillaume Ponçon <gponcon@gmail.com>
2025-01-01 15:10:01 -0300

    Tentative d'utilisation de colmena

--- flake.nix
+++ flake.nix
@@ -1,12 +1,24 @@
 {
   description = "Main NixOS Flake";
 
+# Sera utile avec colmena
+#  nixConfig = {
+#    extra-trusted-substituters = [
+#      "https://cache.garnix.io"
+#      "https://nix-community.cachix.org"
+#    ];
+#  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     
     # Utilisation de home manager comme module flake
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+# NE FONCTIONNE PAS POUR LE MOMENT
+#    colmena.url = "github:zhaofengli/colmena";
+#    colmena.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
@@ -46,5 +58,54 @@
       ];
     };
 
-  };
+# NE FONCTIONNE PAS POUR LE MOMENT
+#
+#    colmenaHive = colmena.lib.makeHive self.outputs.colmena;
+#
+#    # https://github.com/zhaofengli/colmena/issues/60#issuecomment-1510496861
+#    colmena = let conf = self.nixosConfigurations; in {
+#      meta = {
+#        description = "Mes hosts";
+#        nixpkgs = import nixpkgs {
+#          system = "x86_64-linux";
+#          #stateVersion = "25.05";
+#          allowUnfree = true;
+#        };
+#        nodeNixpkgs = builtins.mapAttrs (name: value: value.pkgs) conf;
+#        nodeSpecialArgs = builtins.mapAttrs (name: value: value._module.specialArgs) conf;
+#      } // builtins.mapAttrs (name: value: { imports = value._module.args.modules; }) conf;
+#      
+#      # Conf déploiement colmena
+#      defaults.deployment = {
+#        buildOnTarget = nixpkgs.lib.mkDefault false;
+#        allowLocalDeployment = nixpkgs.lib.mkDefault false;
+#        targetUser = null;
+#      };
+#
+#      # Laptop
+#      nlt = {
+#        deployment = {
+#          tags = [ "desktop" "admin" "local" ];
+#          allowLocalDeployment = true;
+#          #targetHost = "192.168.1.1";
+#          buildOnTarget = true;
+#        };
+#      };
+#
+#      # Test vm
+#      test = {
+#        deployment = {
+#          tags = [ "test" "admin" ];
+#          allowLocalDeployment = false;
+#          #targetHost = "192.168.1.1";
+#          #buildOnTarget = true;
+#        };
+#      };
+#
+##      rpi = { pkgs, ... }: {
+##        nixpkgs.system = "aarch64-linux";
+##      };
+#    };
+
+  }; # outputs
 }
