cae808b8e57718074ba1a80052b7acfcee1057c7
Guillaume Ponçon <gponcon@gmail.com>
2025-01-21 11:05:25 -0300

    UID fixes

--- usr/modules/host/sn-pc.nix
+++ usr/modules/host/sn-pc.nix
@@ -28,6 +28,43 @@ in
       #graphic.music.enable = true;
     };
 
+    # Hardware (wip)
+
+    # Cannot do that here
+    #imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+    boot.initrd.availableKernelModules = [
+      "xhci_pci"
+      "ehci_pci"
+      "ahci"
+      "usb_storage"
+      "sd_mod"
+      "sr_mod"
+      "sdhci_pci"
+    ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-name/NIXOS";
+      fsType = "ext4";
+    };
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-name/BOOT";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+    swapDevices = [ { device = "/dev/disk/by-name/SWAP"; } ];
+
+    networking.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
     # Host specific state version
     system.stateVersion = "24.11";
   };
